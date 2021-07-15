6eb6bd78777505619ee7eba3a94c61825c406700
Michael-dot-com <53223049+Michael-dot-com@users.noreply.github.com>
2021-07-15 09:29:50 +0300

    Update base_page.py

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,3 +1,5 @@
+from selenium.common.exceptions import NoSuchElementException, NoAlertPresentException, TimeoutException
+
 class BasePage():
     def __init__(self, browser, url):
         self.browser = browser
@@ -5,3 +7,14 @@ class BasePage():
 
     def open(self): 
         self.browser.get(self.url)
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except NoSuchElementException:
+            return False
+        return True
+
+    def should_be_login_link(self):
+        assert self.is_element_present(By.CSS_SELECTOR, "#login_link_invalid"), "Login link is not presented"
+    
