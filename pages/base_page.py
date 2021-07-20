8c8b81ce20db19e7c6cf6ab2ca7124254a092ae2
Michael-dot-com <53223049+Michael-dot-com@users.noreply.github.com>
2021-07-20 09:55:55 +0300

    211271
    https://stepik.org/lesson/238819/step/9?unit=211271

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,4 +1,10 @@
+import math
+
+from .locators import BasePageLocators
+from selenium.webdriver.remote.webdriver import WebDriver as RemoteWebDriver
 from selenium.common.exceptions import NoSuchElementException, NoAlertPresentException, TimeoutException
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
 
 class BasePage():
     def __init__(self, browser, url):
@@ -18,3 +24,6 @@ class BasePage():
     def should_be_login_link(self):
         assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
     
+    def go_to_login_page(self):
+        link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        link.click()
