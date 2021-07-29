965ce35be8769189090ea004767ccb1671edfa79
Michael-dot-com <53223049+Michael-dot-com@users.noreply.github.com>
2021-07-29 10:30:11 +0300

    added login user test
    added login user test

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,4 +1,6 @@
 from .base_page import BasePage
+from .locators import LoginPageLocators
+import time
 
 
 class LoginPage(BasePage):
@@ -8,13 +10,20 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        # реализуйте проверку на корректный url адрес
-        assert True
+        assert "login" in self.browser.current_url, '"Login" not in current url'
 
     def should_be_login_form(self):
-        # реализуйте проверку, что есть форма логина
-        assert True
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
 
     def should_be_register_form(self):
-        # реализуйте проверку, что есть форма регистрации на странице
-        assert True
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not presented"
+
+    def register_new_user(self, email, password):
+        input_email = self.browser.find_element(*LoginPageLocators.INPUT_EMAIL)
+        input_email.send_keys(email)
+        input_password = self.browser.find_element(*LoginPageLocators.INPUT_PASSWORD)
+        input_password.send_keys(password)
+        confirm_password = self.browser.find_element(*LoginPageLocators.CONFIRM_PASSWORD)
+        confirm_password.send_keys(password)
+        reg_button = self.browser.find_element(*LoginPageLocators.REG_BUTTON)
+        reg_button.click()
