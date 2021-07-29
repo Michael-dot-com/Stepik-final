047f7036a8fb2c2d23fc9e6ab17cacec3b4b71fd
Michael-dot-com <53223049+Michael-dot-com@users.noreply.github.com>
2021-07-29 10:29:40 +0300

    added login user test
    added login user test

--- pages/locators.py
+++ pages/locators.py
@@ -1,29 +1,31 @@
 from selenium.webdriver.common.by import By
 
 
-class BasePageLocators:
-    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
-    BASKET_BUTTON = (By.CSS_SELECTOR, ".basket-mini a.btn-default")
-    USER_ICON = (By.CSS_SELECTOR, ".icon-user")
-
-
 class LoginPageLocators:
-    LOGIN_FORM = (By.ID, "login_form")
-    REGISTER_FORM = (By.ID, "register_form")
-    REGISTER_EMAIL_INPUT = (By.ID, "id_registration-email")
-    REGISTER_PASSWORD_INPUT_1 = (By.ID, "id_registration-password1")
-    REGISTER_PASSWORD_INPUT_2 = (By.ID, "id_registration-password2")
-    REGISTER_BUTTON_REGISTER = (By.CSS_SELECTOR, "button[name=registration_submit]")
+    LOGIN_FORM = (By.CSS_SELECTOR, ".login_form")
+    REGISTER_FORM = (By.CSS_SELECTOR, ".register_form")
+    INPUT_EMAIL = (By.CSS_SELECTOR, "#id_registration-email")
+    INPUT_PASSWORD = (By.CSS_SELECTOR, "#id_registration-password1")
+    CONFIRM_PASSWORD = (By.CSS_SELECTOR, "#id_registration-password2")
+    REG_BUTTON = (By.NAME, "registration_submit")
 
 
 class ProductPageLocators:
-    ADD_TO_BASKET = (By.CSS_SELECTOR, "#add_to_basket_form button.btn-add-to-basket")
-    NAME_OF_PRODUCT = (By.CSS_SELECTOR, "#content_inner .product_main h1")
-    AMOUNT_OF_PRODUCT = (By.CSS_SELECTOR, "#content_inner .product_main .price_color")
-    ALERT_SUCCESS_NAME_PRODUCT = (By.CSS_SELECTOR, "#messages > :nth-child(1) .alertinner strong")
-    ALERT_SUCCESS_BASKET_AMOUNT = (By.CSS_SELECTOR, "#messages > :nth-child(3) .alertinner strong")
+    ADD_TO_BASKET_BUTTON = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    PRODUCT_NAME = (By.CSS_SELECTOR, ".product_main > h1")
+    PRODUCT_NAME_IN_BASKET = (By.CSS_SELECTOR, "#messages > div:nth-child(1) > div > strong")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main > .price_color")
+    PRODUCT_PRICE_IN_BASKET = (By.CSS_SELECTOR, ".alertinner>p>strong")
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, "#messages > div:nth-child(1)")
+
+
+class BasePageLocators:
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    BASKET_LINK = (By.CSS_SELECTOR, ".btn-group > a")
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user")
 
 
 class BasketPageLocators:
-    BASKET_CONTENT = (By.CSS_SELECTOR, "#basket_formset .basket-items")
-    TEXT_ABOUT_EMPTY = (By.CSS_SELECTOR, "#content_inner p")
+    BASKET_ITEM = (By.CSS_SELECTOR, ".basket-items")
+    BASKET_EMPTY_TEXT = (By.CSS_SELECTOR, "#content_inner>p")
